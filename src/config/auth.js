a712653e71d1e8091ef0ff0b09699ee699c78059
Daniel Oliveira <danielso2007@gmail.com>
2019-04-11 19:15:08 -0300

    Ajustes diversos.

--- src/config/auth.js
+++ src/config/auth.js
@@ -14,7 +14,6 @@ module.exports = (req, res, next) => {
         if(!token) {
             return res.status(403).send({errors: ['No token provided.']});
         }
-        console.log(env.authSecret);
         jwt.verify(token, env.authSecret, function(err, decoded) {
             if(err) {
                 return res.status(403).send({errors: ['Failed to authenticate token.']});
